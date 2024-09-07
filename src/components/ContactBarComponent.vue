309bb01e1acecc25c9431c3a5b65499426b0628e
Ivan Cézanne Seara <ivancezanne@gmail.com>
2024-09-07 00:58:25 -0300

    defines unstyled template

--- src/components/ContactBarComponent.vue
+++ src/components/ContactBarComponent.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+import ContactMeanComponent from './ContactMeanComponent.vue';
+</script>
+
 <template>
-	<div id="contact-bar"></div>
+	<div id="contact-bar">
+		<ContactMeanComponent type="linkedin" link="https://linkedin.com/in/ivancezanne" />
+		<ContactMeanComponent type="github" link="https://github.com/ivandomar" />
+	</div>
 </template>
 
 <style scoped></style>
---
+++ src/components/ContactMeanComponent.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps<{
+	type: 'linkedin' | 'github';
+	link: string;
+}>();
+</script>
+
+<template>
+	<a :href="link" target="blank" class="contact-mean">
+		<img :src="icon" alt="Ícone do meio de contato" class="contact-mean-icon" />
+	</a>
+</template>
+
+<style scoped></style>
+
+<script lang="ts">
+export default {
+	computed: {
+		icon() {
+			switch (this.type) {
+				case 'linkedin':
+					return '@/assets/icons/linkedin-icon.png';
+				case 'github':
+					return '@/assets/icons/github-icon.png';
+				default:
+					return '';
+			}
+		},
+	},
+};
+</script>
